9f7416e70e9cb466bbf04e696ce8d46c50435a30
inno <innocentiahan14@gmail.com>
2025-11-27 22:03:59 +0800

    modify user migration to nullable

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('nomor_hp')->unique();
+            $table->string('nama');
+            $table->string('nomor_hp')->unique()->nullable();
             $table->boolean('verifikasi_wa')->default(false);
-            $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('username')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
 
             $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->foreignId('kelompok_tani_id')->nullable()->constrained('kelompok_tanis')->onDelete('set null');
